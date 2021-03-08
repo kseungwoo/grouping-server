10829eebac3847ce4e6a821dd1ebe5bea8223dcd
김승우 <seungwookim.dev@gmail.com>
2021-03-08 15:22:19 +0900

    update

--- grouping-gw/src/main/java/com/covengers/grouping/configuration/SwaggerConfiguration.java
+++ grouping-gw/src/main/java/com/covengers/grouping/configuration/SwaggerConfiguration.java
@@ -1,15 +1,19 @@
 package com.covengers.grouping.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
 @Configuration
-public class SwaggerConfiguration {
+public class SwaggerConfiguration implements SwaggerResourcesProvider {
 
     @Bean
     public Docket api() {
@@ -19,4 +23,22 @@ public class SwaggerConfiguration {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        resources.add(swaggerResource("grouping-api", "/v2/api-docs/api", "2.0"));
+        resources.add(swaggerResource("grouping-chat", "/v2/api-docs/chat", "2.0"));
+        resources.add(swaggerResource("grouping-gateway", "/v2/api-docs", "2.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
 }
